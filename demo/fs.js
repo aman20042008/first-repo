f59a912080ab48e01db7972fe5f6f8bbbe7ef30a
Aman <aman.kill2008@gmail.com>
2021-06-15 16:11:59 +0600

    update1

--- demo/fs.js
+++ demo/fs.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.mkdir(path.join(path.dirname, 'test'), (err) => {
+fs.mkdir(path.join(__dirname, 'test/test'), (err) => {
     if (err) {
         throw err;
     }
+
+    console.log('папка создана')
 })
